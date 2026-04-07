aa185c3835474a68c22a5e90fd69ddabc32c937c
Pv sai sivaram <sram93815@gmail.com>
2026-04-07 15:18:44 +0530

    COMMIT

---
+++ DAPPER ASSIGNMENTS/Assignment1/Data/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Assignment1.Data
+{
+    public class DbContext
+    {
+        private readonly string _connection = "Data Source=LAPTOP-D08GSENH\\SQLEXPRESS01;Initial Catalog=NewPractisedb;Integrated Security=True;Trust Server Certificate=True;";
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connection);
+        }
+    }
+}
